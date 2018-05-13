23e661594be7c0b408b1e17de2e886b0392c0a64
Hameed Abdul <hameedabdulrashid@gmail.com>
2018-05-12 22:23:26 -0500

    Basic css updates

--- web/hmart.php
+++ web/hmart.php
@@ -32,7 +32,7 @@
         <div class="col-md-10 col-md-offset-1">
 
           <div class="text-center banner-text ">
-             <a class="logo" href="#">한아름 마트십니다 </a>
+             <a class="logo" href="#"></a>
             </p>
           </div>
 
@@ -45,31 +45,38 @@
 <!--intro section -->
 <section id="intro" class="section intro">
   <div class="intro-content">
-      <div class="row">
+  <div class="container">
+    <div class="row">
       <!-- intro --> 
-        <div class="text-center">
-          <h1>한아름 마트 환영합니다 </h1>
-          <h2>Welcome to Hmart</h2>
-          <p>First there was kpop now the third wave of the Korean Invasion... HMart</p>
-        </div>
+      <div class="blockquote text-center">
+        <h1>한아름 마트 환영합니다 </h1>
+        <h2>Welcome to Hmart</h2>
+        <p>First there was kpop now the third wave of the Korean Invasion... HMart</p>
       </div>
-
-      <div class="row">
-          <div class="col-md-3 col-sm-offset-1"> <img src="images/ceo.jpg"> </div>
-          <div class="col-md-6">
-            <h1>Our Founder</h1>
-            <p> 추아섬 (Mr.Chow)</p>
-          </div>
-        </div>
-
-      <div class="row">
-        <div class="col-lg-16"> <a href="top20Store.php">See Top 20 Selling Products At Each Store</a></div>
-        <div class="col-lg-16"> <a href="top20State.php">See Top 20 Selling Products In Each State</a></div>
-        <div class="col-lg-16"> <a href="top5Stores.php">See Top 5 Stores With Most Sales</a></div>
-        <div class="col-lg-16"> <a href="query4.php">See Top Products Compared For Each Store</a></div>
-        <div class="col-lg-16"> <a href="query5.php">See Top Selling Product Types</a></div>
-        
+    </div>
+    
+    <div class="row">
+      <div class="blockquote text-center">
+        <div class="col-lg-22"> <a href="top20Store.php"> <h3>  See Top 20 Selling Products At Each Store  </h3></a></div>
+        <div class="col-lg-22"> <a href="top20State.php"><h3>See Top 20 Selling Products In Each State</h3</a></div>
+        <div class="col-lg-22"> <a href="top5Stores.php"><h3>See Top 5 Stores With Most Sales</h3</a></div>
+        <div class="col-lg-22"> <a href="query4.php"><h3>See Top Products Compared For Each Store</h3</a></div>
+        <div class="col-lg-22"> <a href="query5.php"><h3>See Top Selling Product Types</h3</a></div>
       </div>
+      
+      
+    </div>
+    <div class="text-center">
+      <img src="images/ceo.jpg"> 
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <h1>Our Founder</h1>
+        <p> 추아섬 (Mr.Chow)</p>
+      </div>
+    </div>
+  </div>
+
       </div>
     </div>
         
